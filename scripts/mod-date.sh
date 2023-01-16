2c9f5fc33b9b102cbb729ee1b3e6066c978eb838
LithiumPowered <lithiumt480@gmail.com>
2023-01-15 20:34:07 -0800

    Added a new script for the weather module

---
+++ scripts/mod-date.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+NOT_TIMER=5000 # in miliseconds
+NOT_ID=12
+
+CITY_LIST=( 
+  "Manteca" 
+  "Livermore" 
+)
+
+# Display notification without weather
+NO_TITLE="$(date '+%a, %b %d')"
+dunstify -r $NOT_ID -t $NOT_TIMER "$NO_TITLE" "$NO_BODY" 
+
+# Add weather to current if it runs successfully
+for CITY in ${CITY_LIST[@]}; do
+  if WEATHER="$(timeout 0.5 curl -s "wttr.in/${CITY/ /+}"?format=%C+\(%x\),+%p,+%t)"; then
+    NO_BODY+="<i>$CITY</i>, $WEATHER\n"
+  fi
+done && {
+  # TODO: Lol, this looks fucking ugly
+  # Display notification with appended weather info
+  dunstify -r $NOT_ID -t $NOT_TIMER "$NO_TITLE" "\n$NO_BODY" 
+}
+
