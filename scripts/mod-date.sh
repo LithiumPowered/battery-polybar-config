e095c4df0881fb311fb1d85590f17f735bdd66b6
LithiumPowered <lithiumt480@gmail.com>
2023-01-16 22:04:09 -0800

    2023-01-16: updated date module script

--- scripts/mod-date.sh
+++ scripts/mod-date.sh
@@ -3,18 +3,28 @@
 NOT_TIMER=10000 # in miliseconds
 NOT_ID=$(pidof polybar)
 
+FORMAT="%l:  %w \(%x\), %t\(%f\)"
+CURR_LOC="$(timeout 1 curl -sS "https://ipinfo.io/city")"
+CITY_LIST=(
+  "${CURR_LOC// /+}"
+  "Livermore"
+  "Manteca"
+)
+# Remove possibly repeated elements
+CITY_LIST=$(printf "%s\n" "$CITY_LIST[@]" | sort -u)
+
 # Display notification without weather
-NO_TITLE="$(date '+(%a) %b %d, %Y')"
+NO_TITLE="$( date '+(%a) %b %d, %Y' )"
 dunstify -r $NOT_ID -t $NOT_TIMER \
   "$NO_TITLE" \
   "$NO_BODY" 
 
 # Get Current Location
-if CITY="$(timeout 1 curl -s "https://ipinfo.io/city")"; then
+for CITY in ${CITY_LIST[@]}; do 
 
   # Add weather to current if it runs successfully
-  if WEATHER="$( timeout 1 curl -s "wttr.in/${CITY/ /+}?T" | head -n7 )"; then
-    NO_BODY="$WEATHER"
+  if WEATHER="$( timeout 3 curl -fGsS -H "Accept-Language: ${LANG%_*}" --compressed "https://wttr.in/${CITY// /+}?T&format=${FORMAT// /+}" )"; then
+    NO_BODY+="$WEATHER\n"
 
   # Append notification if curl is successful
   dunstify -r $NOT_ID -t $NOT_TIMER \
@@ -22,4 +32,4 @@ if CITY="$(timeout 1 curl -s "https://ipinfo.io/city")"; then
     "\n${NO_BODY/\\/ }" 
   fi
 
-fi
+done
