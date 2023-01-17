f1e55ef8811af0bda5c17b9b7803ee1d5579182b
LithiumPowered <lithiumt480@gmail.com>
2023-01-17 15:48:55 -0800

    2023-01-17: ipc-split also changes i3-split

--- scripts/ipc-split.sh
+++ scripts/ipc-split.sh
@@ -6,7 +6,9 @@ WIN_Y="$(choose -f x 1 <<< $WIN_D)"
 
 # Change split symbol depending on window dimensions
 [[ $WIN_X -gt $WIN_Y ]] && {
+  i3-msg split horizontal >/dev/null 2>&1
   printf "|"
 } || {
+  i3-msg split vertical >/dev/null 2>&1
   printf "-"
 }
