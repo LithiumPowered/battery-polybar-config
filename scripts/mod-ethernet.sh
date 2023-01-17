03eb9e772eee184a6bea07e96356cf3920f1a146
LithiumPowered <lithiumt480@gmail.com>
2023-01-16 16:10:05 -0800

    Eth module streamlined and ethernet script created

---
+++ scripts/mod-ethernet.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+NOT_TIMER=5000 # in miliseconds
+NOT_ID=$(pidof polybar)
+
+dunstify -r $NOT_ID -t $NOT_TIMER \
+  "${USER^^} Network - Status" \
+  "..."
+
+# Get ipinfo - exit early on failure
+if ! RES=$(curl -s 'https://ipinfo.io/json'); then
+  exit 0 
+fi
+
+# jq the pertinent info
+PUB_IP="$(jq -r '.ip' <<< $RES| xargs -0)"
+LOC_COUNTRY="$(jq -r '.country' <<< $RES | xargs -0)"
+LOC_CITY="$(jq -r '.city' <<< $RES | xargs -0)"
+
+# output to console
+dunstify -r $NOT_ID -t $NOT_TIMER \
+  "${USER^^} Network - Status" \
+  "$PUB_IP $LOC_COUNTRY, $LOC_CITY"
