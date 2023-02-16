58c5082203dda3bcbad412108e73cb5ad88dd148
M-Rani <student.ajc@Gmail.com>
2023-02-16 05:45:28 -0800

    Changed ipc script to mark pages by location

--- scripts/python-i3ipc.py
+++ scripts/python-i3ipc.py
@@ -5,31 +5,38 @@ import subprocess
 
 i3 = Connection()
 
-get_window_props = 'xdotool getwindowfocus getwindowgeometry | rg -o Position.*$ | choose 1'
-set_stack_hook_0 = 'polybar-msg action "#stack.hook.0"'
-set_stack_hook_1 = 'polybar-msg action "#stack.hook.1"'
-set_stack_hook_2 = 'polybar-msg action "#stack.hook.2"'
-
-def set_tiled_indicator(i3, e):
-    focused = i3.get_tree().find_focused()
-
+def get_window_pos():
+    get_window_props = 'xdotool getwindowfocus getwindowgeometry | rg -o Position.*$ | choose 1'
     direct_output = subprocess.check_output(get_window_props, shell=True)
     window_pos = direct_output.decode().replace("\n", "").split(',')
+    return int(window_pos[0])
+
+def set_stack_hook(n):
+    subprocess.check_output('polybar-msg action "#stack.hook.{}"'.format(n), shell=True)
+
+def set_indicator(i3, e):
+    focused = i3.get_tree().find_focused()
 
     if focused == None: 
-        subprocess.check_output(set_stack_hook_0, shell=True)
+        set_stack_hook(0)
     else:
-        if int(window_pos[0]) <= 20: 
-            subprocess.check_output(set_stack_hook_1, shell=True)
+        if get_window_pos() <= 20: 
+            set_stack_hook(1)
+            i3.command('mark Primary')
+
         else: 
-            subprocess.check_output(set_stack_hook_2, shell=True)
+            set_stack_hook(2)
+            i3.command('mark Secondary')
 
 def reset_indicator(i3, e):
-    subprocess.check_output(set_stack_hook_0, shell=True)
+    set_stack_hook(0)
+    for mark in [ 'Primary', 'Secondary' ]:
+        [i3.command('unmark {}'.format(mark))]
 
 i3.on('workspace::focus', reset_indicator)
 i3.on('window::close', reset_indicator)
-i3.on('window::focus', set_tiled_indicator)
+i3.on('workspace::move', set_indicator)
+i3.on('window::focus', set_indicator)
 
 # Main Loop
 i3.main()
